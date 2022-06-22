cebcbe22d3da2f8457c235a1ae96bf38d112c231
sarahnvnt <47983951+sarahnvnt@users.noreply.github.com>
2022-06-22 13:34:26 +0700

    indexx

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const adminRoute = require("./routes/admin");
 const authRoute = require("./routes/auth");
-const provinceRoute = require("./routes/province");
+const adminRoute = require("./routes/admin");
 const cultureRoute = require("./routes/culture");
-
-var cors = require("cors");
-app.use(cors());
+const provinceRoute = require("./routes/province");
 
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB connection Successful"))
+  .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => {
     console.log(err);
   });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/admins", adminRoute);
-app.use("/api/provinces", provinceRoute);
 app.use("/api/cultures", cultureRoute);
+app.use("/api/provinces", provinceRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
