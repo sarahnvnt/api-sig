54ab73cb95b70e061e0fe78203ef1b166636e74f
sarahnvnt <47983951+sarahnvnt@users.noreply.github.com>
2022-07-08 15:45:47 +0700

    benerin login

--- models/Admin.js
+++ models/Admin.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const AdminSchema = new mongoose.Schema(
-    {
-        username: { type: String , required: true, unique: true, min: 3, max: 20 },
-        password: { type: String, required: true, min: 6},
-        },
+  {
+    username: { type: String, required: true, unique: true, min: 3, max: 20 },
+    password: { type: String, required: true, min: 6 },
+  },
 
-    { timestamps: true }
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Admin", AdminSchema);
